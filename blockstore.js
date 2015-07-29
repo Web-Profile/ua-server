54b686a0b8aa1d68736f791517c39349e995e99f
Ubuntu <wp-user@wp-dev1.pixtufvrvmauljeetzlsjuj1kh.dx.internal.cloudapp.net>
2015-07-29 20:39:06 +0000

    blockstore.js

--- blockstore.js
+++ blockstore.js
@@ -1,15 +1,75 @@
 var execSync = require('child_process').execSync;
-var cmd = "c:\Python27\python.exe -m blockstore.blockstore_cli";
+var http = require('http');
+var querystring = require('querystring');
 
-function lookup(id, callback) {
-    var out = execSync(cmd + "lookup " + id);
-    var url;
-    if (id === "existingId") url = "http://somthing.com";
-    else url = null;
-    // parse
-    callback(null, url);
+var appid = "";
+var appsecret = "";
+
+var options =
+  {
+    hostname: "api.onename.com",
+    port: 80,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+function onenameRequest (body, onResult){
+    var req = http.request(options, function (res){
+        res.setEncoding('utf8');
+
+        var output = '';
+        res.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        res.on('end', function() {
+            if (res.statusCode = 200) onResult(null, obj);
+	    else onResult(res.statusCode, obj);
+        });
+
+    });
+
+    req.on('error', function(err) {});
+
+    req.write(querystring.stringify(body));
+    req.end();
 }
 
-function reserve(id, key callback) {
-    var out = 
+function lookup (id, callback)
+{  
+    if (id === "satya") {return callback (null,"http://somthing.com");}
+ 
+    var body = 
+      {
+	  
+      };
+    onenameRequest (body, function (err, obj) {
+      if (err) {
+	  // Look up failed
+      }
+      var url = obj[id].profile.webprofile;
+
+      else url = null;
+      // parse
+      callback(null, url);
+    });
 }
+
+function reserve(id, addr, profile, callback) {
+
+    var body =
+      {
+	"passname": id,
+	"recipient_address": addr,
+	"passcard":{"Webprofile": profile} 
+      };
+    onenameRequest (body, function (err, obj) {
+        if (err){return callback (err)}
+        return callback (body.status, null) 
+    });
+}
+
+export lookup;
+export reserve;
