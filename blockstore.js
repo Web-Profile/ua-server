6c32a25a6bd7d314fb8b97a2bddaa67bd84e00a8
Ubuntu <wp-user@wp-dev1.pixtufvrvmauljeetzlsjuj1kh.dx.internal.cloudapp.net>
2015-07-29 18:25:28 +0000

    removed all traces of god-forsaken python code

--- blockstore.js
+++ blockstore.js
@@ -2,7 +2,7 @@ var execSync = require('child_process').execSync;
 var cmd = "c:\Python27\python.exe -m blockstore.blockstore_cli";
 
 function lookup(id, callback) {
-    //var out = execSync(cmd + "lookup" + id);
+    var out = execSync(cmd + "lookup " + id);
     var url;
     if (id === "existingId") url = "http://somthing.com";
     else url = null;
--- blockstore/dht/README.md
+++
@@ -1,8 +0,0 @@
-openname-dht
-============
-
-Kademlia DHT for Openname data
-
-To run a stand-alone server:
-
-> twistd -noy dht/server.tac
