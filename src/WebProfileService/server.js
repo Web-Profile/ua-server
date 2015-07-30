560063bc60bff6604f6e0438650bc2de3c3638fe
Wael Rabadi <waelra@microsoft.com>
2015-07-29 19:01:09 -0700

    Added cors

--- src/WebProfileService/server.js
+++ src/WebProfileService/server.js
@@ -252,6 +252,11 @@ function createProfile(request, response) {
     });
 }
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', function (request, response) {
     
